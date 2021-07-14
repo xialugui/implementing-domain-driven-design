12b06293f9e108f4612bfbfb5b08af081b3ccdb1
xialugui <xialugui@mimovr.com>
2021-07-14 16:58:09 +0800

    feature(all): 项目初始化

---
+++ user/src/main/java/cn/xialugui/user/UserApplication.java
@@ -0,0 +1,13 @@
+package cn.xialugui.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
