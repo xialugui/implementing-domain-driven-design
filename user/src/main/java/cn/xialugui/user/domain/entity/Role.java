c9e577eca5984e68d3d8a802a0cd0ff283eabd2b
xialugui <xialugui@139.com>
2021-07-14 19:42:44 +0800

    feature(用户): 增加用户聚合根和角色实体

---
+++ user/src/main/java/cn/xialugui/user/domain/entity/Role.java
@@ -0,0 +1,16 @@
+package cn.xialugui.user.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 角色聚合根
+ *
+ * @author 夏露桂
+ * @since 2021/7/14 19:38
+ */
+@Getter
+@Setter
+public final class Role extends Entity {
+    private String name;
+}
