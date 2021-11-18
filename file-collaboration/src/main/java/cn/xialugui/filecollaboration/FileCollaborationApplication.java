e9706e7693ae0619dd8e0eeffd8ef6ad8fabb140
xialugui <xialugui@139.com>
2021-11-19 00:14:59 +0800

    refactor(依赖): 1. 增加依赖文件

--- file-collaboration/src/main/java/cn/xialugui/filecollaboration/FileCollaborationApplication.java
+++ file-collaboration/src/main/java/cn/xialugui/filecollaboration/FileCollaborationApplication.java
@@ -1,11 +1,13 @@
 package cn.xialugui.filecollaboration;
 
+import com.lugew.winsin.web.configuration.EnableStandardRestfulResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableStandardRestfulResponse
 public class FileCollaborationApplication {
 
     public static void main(String[] args) {
