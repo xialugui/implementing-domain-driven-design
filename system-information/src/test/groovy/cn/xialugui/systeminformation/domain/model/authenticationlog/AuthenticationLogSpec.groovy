d6183f1947927825b3fd03b7ebb0a53fe8cf2bbe
xialugui <xialugui@139.com>
2021-12-11 00:19:31 +0800

    test(认证日志): 增加认证失败测试，重构现有用例

--- system-information/src/test/groovy/cn/xialugui/systeminformation/domain/model/authenticationlog/AuthenticationLogSpec.groovy
+++ system-information/src/test/groovy/cn/xialugui/systeminformation/domain/model/authenticationlog/AuthenticationLogSpec.groovy
@@ -1,6 +1,9 @@
 package cn.xialugui.systeminformation.domain.model.authenticationlog
 
+
+import cn.xialugui.systeminformation.domain.model.authenticationlog.command.CreateAuthenticationFailureLogCommand
 import cn.xialugui.systeminformation.domain.model.authenticationlog.command.CreateAuthenticationSuccessLogCommand
+import cn.xialugui.systeminformation.domain.model.authenticationlog.event.AuthenticationFailureLogCreatedEvent
 import cn.xialugui.systeminformation.domain.model.authenticationlog.event.AuthenticationSuccessLogCreatedEvent
 import spock.lang.Subject
 import spock.lang.Title
@@ -9,28 +12,44 @@ import spock.lang.Title
 @Title("认证日志说明")
 class AuthenticationLogSpec extends StubAggregateLifecycleSpecification {
     AuthenticationLogId id = new AuthenticationLogId()
+    def name = "hello";
+    def ip = "127.0.0.1"
+    def remarks = "no"
+    def type = "null"
+    def timestamp = System.currentTimeMillis()
 
     def '认证成功'() {
-        when:
-        def name = "hello";
-        def ip = "127.0.0.1"
-        def remarks = "no"
-        def type = "null"
-        def timestamp = System.currentTimeMillis()
-        AuthenticationLog authenticationLog = new AuthenticationLog(
+        when: '成功'
+        new AuthenticationLog(
                 new CreateAuthenticationSuccessLogCommand(id, name, ip, remarks, type, timestamp))
-        then:
-        with(lifecycle) {
-            appliedEvents.size() == 1
-            appliedEventPayloads[0] == AuthenticationSuccessLogCreatedEvent
-                    .builder()
-                    .authenticationLogId(id)
-                    .name(name)
-                    .remarks(remarks)
-                    .type(type)
-                    .timestamp(timestamp)
-                    .ip(ip)
-                    .build()
-        }
+        then: '触发成功日志'
+        expectEvent(AuthenticationSuccessLogCreatedEvent
+                .builder()
+                .authenticationLogId(id)
+                .name(name)
+                .remarks(remarks)
+                .type(type)
+                .timestamp(timestamp)
+                .ip(ip)
+                .build())
+    }
+
+    def '认证失败'() {
+        when: '失败'
+        new AuthenticationLog(
+                new CreateAuthenticationFailureLogCommand(id, name, ip, remarks, type, timestamp))
+        then: '出发失败日志'
+        expectEvent(
+                AuthenticationFailureLogCreatedEvent
+                        .builder()
+                        .authenticationLogId(id)
+                        .name(name)
+                        .remarks(remarks)
+                        .type(type)
+                        .timestamp(timestamp)
+                        .ip(ip)
+                        .build()
+        )
+
     }
 }
