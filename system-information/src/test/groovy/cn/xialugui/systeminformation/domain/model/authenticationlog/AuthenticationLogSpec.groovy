c1aeeb5faccf902c545517cc16fc2ed4f24641f9
xialugui <xialugui@139.com>
2021-12-11 00:00:12 +0800

    test(聚合测试): 新增Axon聚合测试基类，重构认证日志测试

--- system-information/src/test/groovy/cn/xialugui/systeminformation/domain/model/authenticationlog/AuthenticationLogSpec.groovy
+++ system-information/src/test/groovy/cn/xialugui/systeminformation/domain/model/authenticationlog/AuthenticationLogSpec.groovy
@@ -2,27 +2,15 @@ package cn.xialugui.systeminformation.domain.model.authenticationlog
 
 import cn.xialugui.systeminformation.domain.model.authenticationlog.command.CreateAuthenticationSuccessLogCommand
 import cn.xialugui.systeminformation.domain.model.authenticationlog.event.AuthenticationSuccessLogCreatedEvent
-import org.axonframework.test.aggregate.StubAggregateLifecycleExtension
-import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Title
 
 @Subject
 @Title("认证日志说明")
-class AuthenticationLogSpec extends Specification {
+class AuthenticationLogSpec extends StubAggregateLifecycleSpecification {
     AuthenticationLogId id = new AuthenticationLogId()
-    static StubAggregateLifecycleExtension lifecycle = new StubAggregateLifecycleExtension();
 
-
-    def setup() {
-        lifecycle.activate()
-    }
-
-    def cleanup() {
-        lifecycle.close()
-    }
-
-    def '处理创建认证成功日志命令，发布已创建事件'() {
+    def '认证成功'() {
         when:
         def name = "hello";
         def ip = "127.0.0.1"
@@ -31,8 +19,6 @@ class AuthenticationLogSpec extends Specification {
         def timestamp = System.currentTimeMillis()
         AuthenticationLog authenticationLog = new AuthenticationLog(
                 new CreateAuthenticationSuccessLogCommand(id, name, ip, remarks, type, timestamp))
-
-
         then:
         with(lifecycle) {
             appliedEvents.size() == 1
