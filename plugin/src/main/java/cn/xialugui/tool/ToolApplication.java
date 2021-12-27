f76d5380164bba03ed3595487f083410d2506648
xialugui <xialugui@139.com>
2021-12-27 19:57:00 +0800

    特性(插件): 增加配置

--- plugin/src/main/java/cn/xialugui/tool/ToolApplication.java
+++ plugin/src/main/java/cn/xialugui/tool/ToolApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToolApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ToolApplication.class, args);
     }
-
 }
